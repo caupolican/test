50f9941c7e0e75cf998d3823af8512d9020f4c97
caupolican <caupolican@microsuave.com>
2019-07-28 19:22:19 -0500

    initial commit

---
+++ Spectrum/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using MvvmCross.Core.ViewModels;
+
+namespace Spectrum.ViewModels
+{
+    public class MainViewModel : MvxViewModel
+    {
+        public MainViewModel()
+        {
+        }
+        
+        public override Task Initialize()
+        {
+            //TODO: Add starting logic here
+		    
+            return base.Initialize();
+        }
+
+        public override void Prepare()
+        {
+            Text = "Hellos!";
+
+            base.Prepare();
+        }
+
+        public IMvxCommand ResetTextCommand => new MvxCommand(ResetText);
+        private void ResetText()
+        {
+            Text = "Hello MvvmCross";
+        }
+
+        
+        public string Text { get; set; }
+    }
+}
